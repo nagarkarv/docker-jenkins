0a9b9186e2bc2a09d31fff0a16d407b54b7f35da
Vikram Nagarkar <vikram.nagarkar@yahoo.com>
2018-10-28 08:33:39 +0000

    Added security

--- master/startJenkinsMasterContainer.sh
+++ master/startJenkinsMasterContainer.sh
@@ -1,4 +1,4 @@
 docker stop jenkins_master_DEV
 docker rm jenkins_master_DEV
 
-docker run -d -p 8080:8080 -v $HOME/jenkins_home:/var/jenkins_home --name jenkins_master_DEV jenkins_master_image:v1.0.1
+docker run -d -p 8080:8080 -p 50000:50000 -v $HOME/jenkins_home:/var/jenkins_home --name jenkins_master_DEV jenkins_master_image:v1.0.1
