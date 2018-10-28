7c2d46dd10c4c4d6697ec1126afc79a9bb789519
Vikram Nagarkar <vikram.nagarkar@yahoo.com>
2018-10-28 08:55:50 +0000

    Fixed user

--- master/security.groovy
+++ master/security.groovy
@@ -7,7 +7,15 @@ import jenkins.security.s2m.AdminWhitelistRule
 def instance = Jenkins.getInstance()
  
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
-hudsonRealm.createAccount("admin", "admin")
+
+// only usefull when using docker swarm
+//def user = new File("/run/secrets/jenkins-user").text.trim()
+//def password = new File("/run/secrets/jenkins-pass").text.trim()
+
+def user = "admin"
+def password = "admin"
+
+hudsonRealm.createAccount(user, password)
 instance.setSecurityRealm(hudsonRealm)
  
 def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
