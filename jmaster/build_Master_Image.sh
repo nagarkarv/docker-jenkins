fc861d5a2ca873a4cd6756dabc106cdd21defd3d
Vikram Nagarkar <vikram.nagarkar@yahoo.com>
2019-02-19 12:31:19 +0000

    Added jenkins deployment

--- jmaster/build_Master_Image.sh
+++ jmaster/build_Master_Image.sh
@@ -1,1 +1,1 @@
-docker build -t jenkins_master_image:v1.0.1 .
+docker build -t vikramnagarkar/jenkins_master_image:v1.0.1 .
--- jmaster/startJenkinsMasterContainer.sh
+++ jmaster/startJenkinsMasterContainer.sh
@@ -1,4 +1,4 @@
-docker stop jenkins_master_DEV
-docker rm jenkins_master_DEV
+docker stop vikramnagarkar/jenkins_master_image:v1.0.1 
+docker rm vikramnagarkar/jenkins_master_image:v1.0.1  
 
-docker run -d -p 8080:8080 -p 50000:50000 -v $HOME/jenkins_home:/var/jenkins_home --name jenkins_master_DEV jenkins_master_image:v1.0.1
+docker run -d -p 8080:8080 -p 50000:50000 -v $HOME/jenkins_home:/var/jenkins_home --name  vikramnagarkar-jenkins-master  vikramnagarkar/jenkins_master_image:v1.0.1
