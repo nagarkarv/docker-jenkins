7d1c9fce5ca637c56cb2483cd1a329106e5ff0eb
vikram nagarkar <vikram.nagarkar@yahoo.com>
2019-02-20 12:47:33 +0000

    Added K8s support for agents

--- jmaster/startJenkinsMasterContainer.sh
+++ jmaster/startJenkinsMasterContainer.sh
@@ -1,4 +1,4 @@
-docker stop vikramnagarkar/jenkins_master_image:v1.0.1 
-docker rm vikramnagarkar/jenkins_master_image:v1.0.1  
+docker stop vikramnagarkar/jenkins_master_image:v1.0.2 
+docker rm vikramnagarkar/jenkins_master_image:v1.0.2
 
-docker run -d -p 8080:8080 -p 50000:50000 -v $HOME/jenkins_home:/var/jenkins_home --name  vikramnagarkar-jenkins-master  vikramnagarkar/jenkins_master_image:v1.0.1
+docker run -d -p 8080:8080 -p 50000:50000 -v $HOME/jenkins_home:/var/jenkins_home --name  vikramnagarkar-jenkins-master  vikramnagarkar/jenkins_master_image:v1.0.2
